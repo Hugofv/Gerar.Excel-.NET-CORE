454f26e6881a5aea94553eafa1c287ea771ae64c
Hugo <hugo.fernandes@cogtive.com.br>
2018-08-23 11:56:37 -0300

    Melhoramento

--- Gerar.Excel/Program.cs
+++ Gerar.Excel/Program.cs
@@ -1,6 +1,6 @@
 ﻿
+using System;
 using System.IO;
-using System.Net.Http.Headers;
 using System.Text;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
@@ -11,71 +11,59 @@ namespace Gerar.Excel
     {
         static void Main(string[] args)
         {
-            string sWebRootFolder = @"/home/cogtive/Desktop/";
-            string sFileName = @"demo.xlsx";
-            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            const string folderRoot = @"d:\";
+            const string fileName = @"demo.xlsx";
+            var file = new FileInfo(Path.Combine(folderRoot, fileName));
             if (file.Exists)
             {
                 file.Delete();
-                file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+                file = new FileInfo(Path.Combine(folderRoot, fileName));
             }
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            using (ExcelPackage package = new ExcelPackage(file))
+            using (var package = new ExcelPackage(file))
             {
                 // add a new worksheet to the empty workbook
                 var workbook = package.Workbook;
                 var worksheet = workbook.Worksheets.Add("Equipamento 1");
 
                 //First add the headers
-                worksheet.Cells[1, 1].Value = "ID";
-                worksheet.Cells[1, 2].Value = "Name";
-                worksheet.Cells[1, 3].Value = "Gender";
-                worksheet.Cells[1, 4].Value = "Salary (in $)";
+                worksheet.Cells[5, 2].Value = "Titulo 1";
+                worksheet.Cells[5, 3].Value = "Titulo 2";
+                worksheet.Cells[5, 4].Value = "Titulo 3";
+                worksheet.Cells[5, 5].Value = "Titulo 4";
 
-                worksheet.Cells[1, 1, 1, 4].Style.Font.Bold = true;
-                worksheet.Cells[1, 1, 1, 4].Style.Border.Bottom.Style = ExcelBorderStyle.Double;
+                worksheet.Cells[5, 2, 5, 5].Style.Font.Bold = true;
+                worksheet.Cells[5, 2, 5, 5].Style.Border.Bottom.Style = ExcelBorderStyle.Double;
 
-                //Add values
-                worksheet.Cells["A2"].Value = 1000;
-                worksheet.Cells["B2"].Value = "Jon";
-                worksheet.Cells["C2"].Value = "M";
-                worksheet.Cells["D2"].Value = 5000;
+                for (var i = 0; i < 10; i++)
+                {
+                    var numero = new Random();
 
-                worksheet.Cells["A3"].Value = 1001;
-                worksheet.Cells["B3"].Value = "Graham";
-                worksheet.Cells["C3"].Value = "M";
-                worksheet.Cells["D3"].Value = 10000;
-
-                worksheet.Cells["A4"].Value = 1002;
-                worksheet.Cells["B4"].Value = "Jenny";
-                worksheet.Cells["C4"].Value = "F";
-                worksheet.Cells["D4"].Value = 5000;
+                    worksheet.Cells[i + 6, 2].Value = "Valor: " + numero.Next();
+                    worksheet.Cells[i + 6, 3].Value = "Valor: " + numero.Next();
+                    worksheet.Cells[i + 6, 4].Value = "Valor: " + numero.Next();
+                    worksheet.Cells[i + 6, 5].Value = "Valor: " + numero.Next();
+                }
 
                 worksheet = workbook.Worksheets.Add("Equipamento 2");
 
-                worksheet.Cells[1, 1].Value = "ID";
-                worksheet.Cells[1, 2].Value = "Name";
-                worksheet.Cells[1, 3].Value = "Gender";
-                worksheet.Cells[1, 4].Value = "Salary (in $)";
+                worksheet.Cells[5, 2].Value = "Titulo 1";
+                worksheet.Cells[5, 3].Value = "Titulo 2";
+                worksheet.Cells[5, 4].Value = "Titulo 3";
+                worksheet.Cells[5, 5].Value = "Titulo 4";
 
-                worksheet.Cells[1, 1, 1, 4].Style.Font.Bold = true;
-                worksheet.Cells[1, 1, 1, 4].Style.Border.Bottom.Style = ExcelBorderStyle.Double;
+                worksheet.Cells[5, 2, 5, 5].Style.Font.Bold = true;
+                worksheet.Cells[5, 2, 5, 5].Style.Border.Bottom.Style = ExcelBorderStyle.Double;
 
-                //Add values
-                worksheet.Cells["A2"].Value = 1000;
-                worksheet.Cells["B2"].Value = "Jon";
-                worksheet.Cells["C2"].Value = "M";
-                worksheet.Cells["D2"].Value = 5000;
+                for (var i = 0; i < 10; i++)
+                {
+                    var numero = new Random();
 
-                worksheet.Cells["A3"].Value = 1001;
-                worksheet.Cells["B3"].Value = "Graham";
-                worksheet.Cells["C3"].Value = "M";
-                worksheet.Cells["D3"].Value = 10000;
-
-                worksheet.Cells["A4"].Value = 1002;
-                worksheet.Cells["B4"].Value = "Jenny";
-                worksheet.Cells["C4"].Value = "F";
-                worksheet.Cells["D4"].Value = 5000;
+                    worksheet.Cells[i + 6, 2].Value = "Valor: " + numero.Next();
+                    worksheet.Cells[i + 6, 3].Value = "Valor: " + numero.Next();
+                    worksheet.Cells[i + 6, 4].Value = "Valor: " + numero.Next();
+                    worksheet.Cells[i + 6, 5].Value = "Valor: " + numero.Next();
+                }
 
                 package.SaveAs(file);
             }
